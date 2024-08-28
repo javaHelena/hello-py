69fa861cf1fb611abee77bd49ffbaa22e932a533
Helena Hjertén <helena@hjerten.com>
2024-08-28 13:54:02 +0200

    changes

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World! This is a simple Flask app for sure !."
+    return "Hello, World! This is a simple Flask app for sure. Changes is cool !."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
